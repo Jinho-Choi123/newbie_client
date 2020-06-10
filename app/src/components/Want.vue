851ba716280adc2073e2a27ee3c52f9acbcf86b9
Jinho Choi <choijinho817@gmail.com>
2020-06-11 04:54:45 +0900

    make want.vue, not finished. having some errors.

--- app/src/components/Want.vue
+++ app/src/components/Want.vue
@@ -1,94 +1,100 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+  <div>
+
+    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+      <label for="input-with-list">Sports:</label>
+      <b-form-input required list="input-list" id="input-with-list" v-model="form.sportS"></b-form-input>
+      <b-form-datalist id="input-list" :options="Sports"></b-form-datalist>
+
+      <label for="datepicker">Date:</label>
+      <b-form-datepicker id="datepicker" v-model="form.date" class="mb-2"></b-form-datepicker>
+
+      <label for="start_time_picker">Start Time: </label>
+      <b-input-group class="mb-3">
+        <b-form-input required id="start_time_picker" v-model="form.start_time" type="text" placeholder="HH:mm:ss">
+        </b-form-input>
+        <b-input-group-append>
+          <b-form-timepicker v-model="value" button-only right show-seconds locale="kr" aria-controls="start_time_picker">
+          </b-form-timepicker>
+        </b-input-group-append>
+      </b-input-group>
+
+      <label for="end_time_picker">end Time: </label>
+      <b-input-group class="mb-3">
+        <b-form-input required id="end_time_picker" v-model="form.end_time" type="text" placeholder="HH:mm:ss">
+        </b-form-input>
+        <b-input-group-append>
+          <b-form-timepicker v-model="form.end_time" button-only right show-seconds locale="kr" aria-controls="end_time_picker">
+          </b-form-timepicker>
+        </b-input-group-append>
+      </b-input-group>
+
+      <label for="input-with-list2">Place:</label>
+      <b-form-input list="input-list2" id="input-with-list2" v-model="form.place" required></b-form-input>
+      <b-form-datalist id="input-list2" :options="Places"></b-form-datalist>
+
+      <label for="group_limit">Group Limit: </label>
+      <b-form-input id="group_limit" v-model="form.group_limit" placeholder="5" ></b-form-input>
+
+      <b-form-textarea id="textarea" v-model="form.comment" placeholder="Comment" rows="3" max-rows="6"></b-form-textarea>
+
+      <b-button type="submit" variant="primary">Submit</b-button>
+      <b-button type="reset" variant="danger">Reset</b-button>
+    </b-form>
+
   </div>
 </template>
 
 <script>
+
+import axios from 'axios'
+
 export default {
-  name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      form: {
+        sports: '',
+        date: '',
+        group_limit: '5',
+        comment: '',
+        start_time: '',
+        end_time: '',
+        place: '',
+        register_user_id: '23'
+      },
+      Sports: ['baseball', 'basketball', 'soccer', 'tennis'],
+      Places: ['SportsComplex', 'North SchoolYard', 'Main SchoolYard', 'Swimming Pool'],
+      show: true
+    }
+  },
+  methods: {
+    onSubmit (evt) {
+      console.log('entered submit method')
+      evt.preventDefault()
+      const data = this.form
+      axios.post('http://localhost:8080/want/data', data)
+        .then(res => {
+          alert('your request has been successfully submitted.')
+        })
+        .catch((err) => {
+          alert('Error! Please try it again.')
+          console.log(err)
+        })
+    },
+    onReset (evt) {
+      evt.preventDefault()
+      const data = this.form
+      this.show = false
+      data.sports = ''
+      data.place = ''
+      data.date = ''
+      data.group_limit = '5'
+      data.start_time = ''
+      data.end_time = ''
+      data.comment = ''
+      this.$nextTick(() => {
+        this.show = true
+      })
     }
   }
 }
